0895852871505e2010a3d6970bdf1acf69797d30
Jackson Uptain <jackson.uptain@gmail.com>
2023-12-01 17:41:19 -0500

    Add files via upload

--- JacksonUptainPortfolio/main.js
+++ JacksonUptainPortfolio/main.js
@@ -1,1 +1,9 @@
 //Ended up not using this file.
+$('.moreless-button').click(function() {
+    $('.moretext').slideToggle();
+    if ($('.moreless-button').text() == "Read more") {
+      $(this).text("Read less")
+    } else {
+      $(this).text("Read more")
+    }
+  });
